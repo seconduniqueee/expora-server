441572a488c074128c35201ac443d16e0313cd27
secondUniqueee <plotnikovfw@gmail.com>
2024-11-12 21:42:24 +0200

    Chore: set initial connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::net::TcpListener;
+
+const TCP_ADDRESS: &'static str = "127.0.0.1";
+const PORT: i32 = 7878;
+
 fn main() {
-    println!("Hello, world!");
+    let address = format!("{TCP_ADDRESS}:{PORT}");
+    let listener = TcpListener::bind(address).unwrap();
+
+    println!("App is running, listening on the port {PORT}");
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
